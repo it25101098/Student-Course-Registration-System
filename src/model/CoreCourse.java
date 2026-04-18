b9b5e98f2edf6f0b95f30ab792ae733faaa7c62d
oshadakaveesh2022 <oshadakaveesh2022@gmail.com>
2026-04-18 15:54:46 +0530

    Update the Parameterized Constructor in the CoreCourse class

--- src/model/CoreCourse.java
+++ src/model/CoreCourse.java
@@ -3,6 +3,11 @@ package model;
 public class CoreCourse extends Course {
     //Attributes
     private String department; // instance variables
+    //Parameterized Constructor
+    CoreCourse(String department, String courseId , String courseName, int credits ){
+        super(courseId, courseName ,credits);
+        this.department=department;
+    }
 
 
 
