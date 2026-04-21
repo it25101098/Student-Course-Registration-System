0b3d505c18f64ff7d5ce23864bd7daf3b6a3531f
oshadakaveesh2022 <oshadakaveesh2022@gmail.com>
2026-04-21 21:42:31 +0530

    List to store courses in memory

--- src/service/CourseService.java
+++ src/service/CourseService.java
@@ -8,4 +8,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CourseService {
+    private ArrayList<Course> courseList = new ArrayList<>();
+    private static final String FILE_NAME ="courses.txt";
+    Scanner scanner = new Scanner(System.in);
+
 }
