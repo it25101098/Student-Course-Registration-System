9af39ffe81d76cf637943c4d689ae82bee3324cf
oshadakaveesh2022 <oshadakaveesh2022@gmail.com>
2026-04-21 20:15:33 +0530

    Import the Packages

--- src/service/CourseService.java
+++ src/service/CourseService.java
@@ -1,4 +1,11 @@
 package service;
+import model.CoreCourse;
+import model.Course;
+import model.ElectiveCourse;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CourseService {
 }
