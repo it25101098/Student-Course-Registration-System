a21bd3840c67320fa15c5c81b5b31b900ff2fd15
oshadakaveesh2022 <oshadakaveesh2022@gmail.com>
2026-04-21 19:46:08 +0530

    ElectiveCourse class inherit from Course class and update the parameterized Constructor in ElectiveCourse class

--- src/model/ElectiveCourse.java
+++ src/model/ElectiveCourse.java
@@ -1,8 +1,14 @@
 package model;
 
-public class ElectiveCourse {
+public class ElectiveCourse extends Course {
     //Attributes
     private String specialization;
-    
+
+    //Parameterized Constructor
+    public ElectiveCourse(String courseId,String courseName,int credits, String specialization){
+        super(courseId, courseName, credits);
+        this.specialization=specialization;
+    }
+
 
 }
